1ab0d0954ac1151b84a12336718a821f3a392a6d
Vadym Hutei <vhutei@alliancedigital.tech>
2024-07-12 11:50:33 +0300

    format

--- app/main.py
+++ app/main.py
@@ -5,7 +5,30 @@ from tabulate import tabulate
 
 data_dir = '../data'
 time_format = '%Y-%m-%d %H:%M'
+min_c = 10
+max_c = 50
 
+RESET = "\033[0m"
+BLACK = "\033[30m"
+RED = "\033[31m"
+GREEN = "\033[32m"
+YELLOW = "\033[33m"
+BLUE = "\033[34m"
+MAGENTA = "\033[35m"
+CYAN = "\033[36m"
+WHITE = "\033[37m"
+
+# Bright colors
+BRIGHT_BLACK = "\033[90m"
+BRIGHT_RED = "\033[91m"
+BRIGHT_GREEN = "\033[92m"
+BRIGHT_YELLOW = "\033[93m"
+BRIGHT_BLUE = "\033[94m"
+BRIGHT_MAGENTA = "\033[95m"
+BRIGHT_CYAN = "\033[96m"
+BRIGHT_WHITE = "\033[97m"
+
+# Background colors
 BG_BLACK = "\033[40m"
 BG_GRAY = "\033[100m"
 BG_RED = "\033[41m"
@@ -16,7 +39,15 @@ BG_MAGENTA = "\033[45m"
 BG_CYAN = "\033[46m"
 BG_WHITE = "\033[47m"
 
-RESET = "\033[0m"
+# Bright background colors
+BG_BRIGHT_BLACK = "\033[100m"
+BG_BRIGHT_RED = "\033[101m"
+BG_BRIGHT_GREEN = "\033[102m"
+BG_BRIGHT_YELLOW = "\033[103m"
+BG_BRIGHT_BLUE = "\033[104m"
+BG_BRIGHT_MAGENTA = "\033[105m"
+BG_BRIGHT_CYAN = "\033[106m"
+BG_BRIGHT_WHITE = "\033[107m"
 
 def get_data(file_name) -> dict:
     data = {}
@@ -39,12 +70,12 @@ def get_data(file_name) -> dict:
     return dict(sorted(data.items()))
 
 def get_colour(count: int) -> str:
-    colour = BG_GRAY
+    colour = BG_GRAY + BLACK
 
-    if 0 <= count <= 15:
-        colour = BG_BLACK
-    elif 45 < count <= 60:
-        colour = BG_WHITE
+    if 0 <= count <= min_c:
+        colour = BG_BLACK + WHITE
+    elif max_c < count <= 60:
+        colour = BG_WHITE + BLACK
 
     return colour
 
@@ -57,14 +88,14 @@ def get_table(data: dict) -> list[list[str]]:
         day = [day_name]
         for i in range(24):
             count = day_data.get(i, 0)
-            day.append(f'{get_colour(count)}{count}{RESET}')
+            day.append(f'{get_colour(count)}{count:^4d}{RESET}')
 
         table.append(day)
 
     return table
 
 if __name__ == '__main__':
-    file_name = '20240709_11'
+    file_name = '20240701_12'
     data = get_data(file_name)
     table = get_table(data)
-    print(tabulate(table, headers='firstrow'))
+    print(tabulate(table, headers='firstrow', tablefmt='fancy_grid'))
